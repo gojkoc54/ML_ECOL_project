2d400e7bf6144829e37d770bd0215a5ecc4f0d0f
Gojko Cutura <gojkoc54@gmail.com>
2021-12-23 07:13:45 +0100

    changed the name of the script inside run_train.sh

--- src/run_train.sh
+++ src/run_train.sh
@@ -2,13 +2,7 @@ models=( "alexnet" "vgg16" "resnet18" "densenet121" )
 
 for model in "${models[@]}"
 do
-	python classification_playground.py --model ${model}	
+	python train_cnn_model.py --model ${model} --epochs 10 --lr 0.0001
 done
-
-
-
-
-
-
 
 
